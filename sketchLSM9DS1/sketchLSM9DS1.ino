909a60eb21c9e3877b0f3447f4130b12e1ed8264
Rukakuka <59419841+Rukakuka@users.noreply.github.com>
2020-05-12 23:13:13 +0300

    Update

--- sketchLSM9DS1/sketchLSM9DS1.ino
+++ sketchLSM9DS1/sketchLSM9DS1.ino
@@ -30,7 +30,8 @@ XmlMessage Message;
 
 void setup() {
 
-    Serial.begin(115200);
+    Serial.begin(2000000);
+
     Bus.begin();
     Bus.setSpeed(true);
     Bus.timeOut(1000);
@@ -41,8 +42,8 @@ void setup() {
     Sensor.Set_ORIENT_CFG_G(LSM9DS1::GyroAxisSign::Negative, LSM9DS1::GyroAxisSign::Positive, LSM9DS1::GyroAxisSign::Positive, LSM9DS1::GyroAxisOrder::XYZ);
     
     Sensor.Set_REG5_XL(LSM9DS1::AccelDecimation::NoDecimation);
-    Sensor.Set_REG6_XL(LSM9DS1::AccelODR::ODR_238, LSM9DS1::AccelFullscale::G_16, LSM9DS1::AccelAntiAliasingBandwidth::BW_50);
-    Sensor.Set_REG7_XL(LSM9DS1::AccelHighResolutionMode::Enabled, LSM9DS1::AccelFilterCutoff::ODR_TO_400, LSM9DS1::AccelDataSelection::Filtered);
+    Sensor.Set_REG6_XL(LSM9DS1::AccelODR::ODR_238, LSM9DS1::AccelFullscale::G_16, LSM9DS1::AccelAntiAliasingBandwidth::BW_408);
+    Sensor.Set_REG7_XL(LSM9DS1::AccelHighResolutionMode::Enabled, LSM9DS1::AccelFilterCutoff::ODR_TO_9, LSM9DS1::AccelDataSelection::Bypassed);
 
     Sensor.Set_REG1_M(LSM9DS1::MagnetTempCompensation::Enabled, LSM9DS1::MagnetAxisOperativeMode::UltraHighPerfomance, LSM9DS1::MagnetODR::ODR_80, LSM9DS1::MagnetFastODRMode::Enabled);
     Sensor.Set_REG2_M(LSM9DS1::MagnetFullscale::GAUSS_16);
@@ -68,18 +69,29 @@ void setup() {
     Message.AddTag(Message.toString(GET_VAR_NAME(mz)), Message.toString(mz));  
 }
 
-int i = 0 ;
+unsigned long oldtime = 0;
+unsigned long newtime = 0;
+
 void loop()
 {
-    i++;
     if (Sensor.Update() == LSM9DS1::Error::None)
     {
-       
+        /*
+        ax = -32768;
+        ay = -32768;
+        az = -32768;
+        gx = -32768;
+        gy = -32768;
+        gz = -32768;
+        mx = -32768;
+        my = -32768;
+        mz = -32768;
+        
+        /*
         Message.SetValueByTag(GET_VAR_NAME(ax), Message.toString(ax));
         Message.SetValueByTag(GET_VAR_NAME(ay), Message.toString(ay));
         Message.SetValueByTag(GET_VAR_NAME(az), Message.toString(az));
-
-        
+ 
         Message.SetValueByTag(GET_VAR_NAME(gx), Message.toString(gx));
         Message.SetValueByTag(GET_VAR_NAME(gy), Message.toString(gy));
         Message.SetValueByTag(GET_VAR_NAME(gz), Message.toString(gz));
@@ -88,33 +100,39 @@ void loop()
         Message.SetValueByTag(GET_VAR_NAME(my), Message.toString(my));
         Message.SetValueByTag(GET_VAR_NAME(mz), Message.toString(mz));
 
-        Serial.println(Message.GetString().c_str());
-        Serial.println("---------------------");
-
-        //Serial.print("X=");
-        //Serial.print(Sensor.Gyroscope.X);
-        //Serial.print("      Y=");
-        //Serial.print(Sensor.Gyroscope.Y);
-        //Serial.print("      Z=");
-        //Serial.print(Sensor.Gyroscope.Z);
-        //Serial.println();
-        //Serial.print("X=");
-
-        Serial.print(Sensor.Accelerometer.X);
-        Serial.print("      Y=");
-        Serial.print(ay);
-        Serial.print("      Z=");
-        Serial.print(az);
-        Serial.println();
-        /*
-        Serial.print("X=");
-        Serial.print(Sensor.Magnetometer.X);
-        Serial.print("      Y=");
-        Serial.print(Sensor.Magnetometer.Y);
-        Serial.print("      Z=");
-        Serial.print(Sensor.Magnetometer.Z);
-        Serial.println();
+        Message.GetString().c_str();
+        //Serial.println(Message.GetString().c_str());
         */
+        //Serial.println("---------------------");
+/*
+        Serial.print("gx");
+        Serial.print(Sensor.Gyroscope.X);
+        Serial.print("gy");
+        Serial.print(Sensor.Gyroscope.Y);
+        Serial.print("gz");
+        Serial.print(Sensor.Gyroscope.Z);  
+        */
+        Serial.print("ax");      
+        Serial.print(Sensor.Accelerometer.X);
+        /*
+        Serial.print("ay");
+        Serial.print(Sensor.Accelerometer.Y);
+        Serial.print("az");
+        Serial.print(Sensor.Accelerometer.Z);     
+        Serial.print("mx");
+        Serial.print(Sensor.Magnetometer.X);
+        Serial.print("my");
+        Serial.print(Sensor.Magnetometer.Y);
+        Serial.print("mz");
+        Serial.print(Sensor.Magnetometer.Z);
+        */
+        Serial.print("\n");
+        
     }
-    delay(1000);
+
+    Serial.print("t");
+    newtime = millis();
+    Serial.print(newtime - oldtime);
+    oldtime = newtime;
+    delay(100);
 }
